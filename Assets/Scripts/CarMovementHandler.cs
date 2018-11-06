b2e5442aeddf75f66bea4beb90b4b913791d0adf
Ali Alaei <aainsrm@gmail.com>
2018-11-06 18:18:07 +0330

    initial car movement implemented

--- Assets/Scripts/CarMovementHandler.cs
+++ Assets/Scripts/CarMovementHandler.cs
@@ -2,8 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
 
+public class CarMovementHandler : MonoBehaviour {
+    private const float defaultSpeed = 5.0f;
+    private float carSpeed = defaultSpeed; 
+    private void CarSpeedController()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            carSpeed = carSpeed / 2;
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            carSpeed = defaultSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            carSpeed = defaultSpeed * 2;
+        }
+
+        transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
+
+    }
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +32,7 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        CarSpeedController();
+    }
 }
